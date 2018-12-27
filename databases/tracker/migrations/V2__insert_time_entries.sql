af681b2f9dbae4f10ec10265383841d2c365e6f8
Michael Edoror <medoror@michaels-mbp-2.attlocal.net>
2018-12-26 19:31:27 -0500

    adding default time entries

---
+++ databases/tracker/migrations/V2__insert_time_entries.sql
@@ -0,0 +1,6 @@
+insert into time_entries (id, project_id, user_id, date, hours) values (1, '111', '1111', '2017-01-08', 8);
+insert into time_entries (id, project_id, user_id, date, hours) values (2, '222', '2222', '2017-01-08', 8);
+insert into time_entries (id, project_id, user_id, date, hours) values (3, '333', '3333', '2017-01-08', 8);
+insert into time_entries (id, project_id, user_id, date, hours) values (4, '444', '4444', '2017-01-08', 8);
+insert into time_entries (id, project_id, user_id, date, hours) values (5, '555', '5555', '2017-01-08', 8);
+insert into time_entries (id, project_id, user_id, date, hours) values (6, '666', '6666', '2017-01-08', 8);
