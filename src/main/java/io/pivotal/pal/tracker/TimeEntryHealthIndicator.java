4f90df2813e850bf415a75c00968f9dd2e32b118
Michael Edoror <medoror@michaels-mbp-2.attlocal.net>
2018-12-26 19:01:37 -0500

    adding files

---
+++ src/main/java/io/pivotal/pal/tracker/TimeEntryHealthIndicator.java
@@ -0,0 +1,30 @@
+package io.pivotal.pal.tracker;
+
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TimeEntryHealthIndicator implements HealthIndicator {
+
+    private static final int MAX_TIME_ENTRIES = 5;
+    private TimeEntryRepository timeEntryRepository;
+
+    public TimeEntryHealthIndicator( TimeEntryRepository timeEntryRepository){
+        this.timeEntryRepository = timeEntryRepository;
+    }
+    @Override
+    public Health health() {
+
+        Health.Builder builder = new Health.Builder();
+        long size = timeEntryRepository.list().size();
+
+        if( size < MAX_TIME_ENTRIES )
+            builder.down();
+        else
+            builder.up();
+
+        return builder.build();
+    }
+}
